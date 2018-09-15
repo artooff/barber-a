70fd6f14c8c69fe3da61f4c0cc9defc9e8bef959
Андрей <artov200270@gmail.com>
2018-09-15 14:05:11 +0300

    верстка

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,47 @@
+'use strict'
+
 var gulp = require("gulp");
 var less = require("gulp-less");
+var gulp = require('gulp');
+var debug = require('gulp-debug');
+var watch = require('gulp-watch');
+var server = require("browser-sync").create();
 
-gulp.task("less", function(){
-	gulp.src("less/style.less")
+
+
+gulp.task("style", function(){
+	return gulp.src("less/style.less")
+	.pipe(debug())
 	.pipe(less())
+	.pipe(debug())
 	.pipe(gulp.dest("css"));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('less/**/*.*', gulp.series('style'));
 })
+
+
+gulp.task("serve",  function(){
+	server.init({
+		server:"."
+	});
+
+	server.watch('.').on("change", server.reload);
+})
+
+
+
+gulp.task('dev',
+	gulp.series('style', gulp.parallel('watch', 'serve'))
+);
+
+
+
+
+
+
+
+	
+
 
